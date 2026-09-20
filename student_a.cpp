f08544a0f8d63d166cd7cefc9b8a3e0a1a6fa161
Egor <serovegor575@gmail.com>
2026-09-20 18:13:10 +0300

    Fix duplicate definition in student_a.cpp and verify Cardano solver

--- student_a.cpp
+++ student_a.cpp
@@ -1,12 +1,4 @@
 #include "shared_types.h"
-
-// Заглушка для Студента А (Метод Кардано)
-std::unique_ptr<Result> calculateA(std::shared_ptr<const InputData> data) {
-    auto res = std::make_unique<Result>();
-    return res;
-}
-
-#include "shared_types.h"
 #include <cmath>
 #include <numbers>
 #include <algorithm>
--- student_b.cpp
+++ student_b.cpp
@@ -1,7 +1,7 @@
 #include "shared_types.h"
 
-// Заглушка для Студента Б (Метод Ньютона)
 std::unique_ptr<Result> calculateB(std::shared_ptr<const InputData> data) {
-    auto res = std::make_unique<Result>();
-    return res;
+    auto result = std::make_unique<Result>();
+    result->iterations = 0;
+    return result;
 }
